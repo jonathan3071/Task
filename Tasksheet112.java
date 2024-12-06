53fd0fa4d79202add8a5ce332d9f041481985915
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2024-12-06 12:43:16 +0000

    Primitive Types

---
+++ Tasksheet112.java
@@ -0,0 +1,14 @@
+public class Tasksheet112 {
+    public static void main(String[] args) {
+        String name = "H";
+        long num1 = 3;
+        byte one = 1;
+        short zero = 0;
+        String ww = "w";
+        String rr = "r";
+        String dd = "d";
+        double two = 2.0;
+        boolean tro = true;
+        System.out.println(name + num1 + one + one + zero + " " + ww + zero + rr + one + dd + " " + two + " " + tro);
+    }
+}
