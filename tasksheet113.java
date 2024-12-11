6303d484e0b0519930bf1bfda59e1ae02775b84c
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2024-12-11 06:29:40 +0000

    Task113

---
+++ tasksheet113.java
@@ -0,0 +1,24 @@
+public class tasksheet113 {
+
+    public static void main (String[] args){
+          
+        int total = 5 * 10;
+        System.out.println(total);
+
+        total = total - 25;
+        System.out.println(total);
+
+        total = total * 4; 
+        System.out.println(total);
+
+        total = total / 2;
+        System.out.println(total);
+
+        total = total + 10;
+        System.out.println(total);
+
+        total = total % 7;
+        System.out.println(total);
+
+    }
+}
