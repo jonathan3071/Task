7156c210e190597d7e5363dd6b34ab6e333bcbda
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2024-12-12 00:17:12 +0000

    Task114

--- Tasksheet114.java
+++ Tasksheet114.java
@@ -1,1 +1,10 @@
+public class Tasksheet114 {
 
+    public static void main(String[] args) {
+        int check_number = 10;
+        for (int i = 1; i <= check_number; i++) {
+            String message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+            System.out.println(message);
+        }
+    }
+}
