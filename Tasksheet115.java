b2740f67206e0772ac80560b4ffecc056f97baaa
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2024-12-16 23:46:30 +0000

    Tasksheet115

--- Tasksheet115.java
+++ Tasksheet115.java
@@ -1,1 +1,18 @@
+public class Tasksheet115 {
+    public static void main(String[] args) {
+
+        String a = "Wow";
+        String b = "Wow";
+        String c = new String("Wow");
+        String d = b;
+
+        boolean b1 = a == b;              
+        boolean b2 = d.equals(b + "!");   
+        boolean b3 = !c.equals(a);         
+
+        if (b1 == b2 == b3) {
+             System.out.println("Success!");
+        }
+    }
+}
 
