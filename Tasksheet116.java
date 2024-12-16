fb5f6a7ff0be06d6c58705f01edad9e0137b2d38
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2024-12-16 23:59:55 +0000

    Tasksheet116

--- Tasksheet116.java
+++ Tasksheet116.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Tasksheet116 {
+    public static void main(String[] args) {
+        Scanner txt = new Scanner(System.in);
 
+        
+        System.out.print("Enter a string: ");
+        String input = txt.nextLine();
+
+        
+        StringBuilder reversedString = new StringBuilder(input).reverse();
+
+        
+        if (input.equals(reversedString.toString())) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+        txt.close();
+    }
+}
