5cd42d3191ad95fd0a0b51fdb1dde0f3e97deec1
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2025-01-06 00:19:33 +0000

    Tasksheet120

--- Tasksheet120.java
+++ Tasksheet120.java
@@ -1,1 +1,25 @@
+public class Tasksheet120 {
 
+    // Method that takes variable number of integer arguments
+    public static int Sum(int... numbers) {
+        int totalSum = 0;
+        for (int number : numbers) {
+            int cumulativeSum = 0;
+            
+            for (int i = 1; i <= number; i++) {
+                cumulativeSum += i;
+            }
+            totalSum += cumulativeSum;
+            
+            System.out.println("Cumulative sum for " + number + " = " + cumulativeSum);
+        }
+
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        int total = Sum(4, 5, 10);
+        System.out.println("Total sum = " + total);
+    }
+}
