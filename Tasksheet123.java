cb09e12c5578facb1ef622b64be2c0152a8e8ca5
jonathan3071 <83941501+jonathan3071@users.noreply.github.com>
2025-01-10 06:35:25 +0000

    Tasksheet123

--- Tasksheet123.java
+++ Tasksheet123.java
@@ -1,11 +1,9 @@
-// Book.java
 class Book {
     private String title;
     private String author;
     private int yearPublished;
     private double price;
 
-    // Constructor to initialize the attributes
     public Book(String title, String author, int yearPublished, double price) {
         this.title = title;
         this.author = author;
@@ -13,7 +11,6 @@ class Book {
         this.price = price;
     }
 
-    // toString method to return the book details
     @Override
     public String toString() {
         return "Title: \"" + title + "\"\n" +
@@ -23,15 +20,12 @@ class Book {
     }
 }
 
-// BookTest.java
 public class Tasksheet123  {
     public static void main(String[] args) {
-        // Instantiate three Book objects with different values
         Book book1 = new Book("Java Programming", "John Smith", 2021, 39.99);
         Book book2 = new Book("Python Basics", "Jane Doe", 2020, 29.99);
         Book book3 = new Book("C++ Essentials", "Michael Johnson", 2019, 49.99);
 
-        // Print out the details of each book
         System.out.println("Book 1:\n" + book1 + "\n");
         System.out.println("Book 2:\n" + book2 + "\n");
         System.out.println("Book 3:\n" + book3 + "\n");
